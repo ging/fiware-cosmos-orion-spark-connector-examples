1acd82c87f65d54b35bb6d97cf9f97ce5b60222f
Cesar Valencia <cesar.valencia.blancas@alumnos.upm.com>
2019-04-23 20:02:00 +0200

    Add xample 4 and 6

--- src/main/scala/org/fiware/cosmos/orion/spark/connector/examples/example5/Example5.scala
+++ src/main/scala/org/fiware/cosmos/orion/spark/connector/examples/example5/Example5.scala
@@ -1,39 +1,33 @@
-//package org.fiware.cosmos.orion.flink.connector.examples.example5
-//
-//import org.apache.flink.api.common.functions.AggregateFunction
-//import org.apache.flink.streaming.api.scala._
-//import org.apache.flink.streaming.api.windowing.time.Time
-//import org.fiware.cosmos.orion.flink.connector.OrionSource
-//
-///**
-//  * Example5 Orion Connector
-//  * @author @sonsoleslp
-//  */
-//object Example5{
-//
-//  def main(args: Array[String]): Unit = {
-//    val env = StreamExecutionEnvironment.getExecutionEnvironment
-//    // Create Orion Source. Receive notifications on port 9001
-//    val eventStream = env.addSource(new OrionSource(9001))
-//    // Process event stream
-//    val processedDataStream = eventStream
-//      .flatMap(event => event.entities)
-//      .map(entity => {
-//        entity.attrs("information").value.asInstanceOf[Map[String, Any]]
-//      })
-//      .map(list => list("buses").asInstanceOf[List[Map[String,Any]]])
-//      .flatMap(bus => bus )
-//      .map(bus =>
-//        new Bus(bus("name").asInstanceOf[String], bus("price").asInstanceOf[ scala.math.BigInt].intValue()))
-//      .keyBy("name")
-//      .timeWindow(Time.seconds(5), Time.seconds(2))
-//      .min("price")
-//
-//    // print the results with a single thread, rather than in parallel
-//
-//    processedDataStream.print().setParallelism(1)
-//
-//    env.execute("Socket Window NgsiEvent")
-//  }
-//  case class Bus(name: String,  price: Int)
-//}
+package org.fiware.cosmos.orion.spark.connector.examples.example5
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.fiware.cosmos.orion.spark.connector.OrionReceiver
+
+
+/**
+  * Example4 Orion Connector
+  * @author @sonsoleslp
+  */
+object Example5{
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[4]").setAppName("AvgTemperature")
+    val ssc = new StreamingContext(conf, Seconds(10))
+    ssc.checkpoint("./output")
+    // Create Orion Source. Receive notifications on port 9001
+    val eventStream = ssc.receiverStream(new OrionReceiver("localhost", 9001))
+
+    // Process event stream
+    val processedDataStream = eventStream
+      .flatMap(event => event.entities)
+      .map(ent => ent.id -> {(ent.attrs("temperature").value.asInstanceOf[Number].floatValue(),1L)} )
+      .reduceByKeyAndWindow((acc:(Float,Long),value:(Float,Long))=>{  (acc._1 + value._1, acc._2 + value._2)}, Seconds(10))
+      .map((agg :  (String,(Float,Long))) =>  (agg._1, agg._2._1 / agg._2._2))
+
+    processedDataStream.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
+
